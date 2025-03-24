764a3a0d945302c4b4c0a0a9e8126e7a1f3c43c7
REDLANTERNDEV <87618221+REDLANTERNDEV@users.noreply.github.com>
2025-03-25 00:16:46 +0300

    Enhance TC number validation in Personel.cs

--- Universite Personel Sistemi/Personel.cs
+++ Universite Personel Sistemi/Personel.cs
@@ -63,17 +63,26 @@ namespace Universite_Personel_Sistemi
 
         private bool TcDogrulama(string _tcNo)
         {
-            if (_tcNo.Length != 11) return false;
-            if (!_tcNo.All(char.IsDigit)) return false;
-            if (_tcNo[0] == '0') return false;
+            if (_tcNo.Length != 11) return false; // 1 – TC Kimlik Numaraları 11 karakter olmak zorundadır.
+            if (!_tcNo.All(char.IsDigit)) return false; // 2 – Her hanesi bir rakam olmaldır.
+            if (_tcNo[0] == '0') return false;// 3 – İlk hanesi 0 (sıfır) olamaz
 
-            int[] digits = _tcNo.Select(x => x - '0').ToArray();
+            /* 4 –   1, 3, 5, 7, 9 basamaklarının toplamının 7 katından,
+             2, 4, 6, 8 basamaklarının toplamını çıkarttığımızda elde ettiğimiz sonucun
+            10’a bölümünden kalan sayı(MOD10)  10.basamaktaki sayıyı vermelidir. */
+            int[] digits = new int[_tcNo.Length];  
+
+            for (int i = 0; i < _tcNo.Length; i++)
+            {
+                digits[i] = _tcNo[i] - '0';  
+            }
+
             int teklerinToplami = digits[0] + digits[2] + digits[4] + digits[6] + digits[8];
             int ciftlerinToplami = digits[1] + digits[3] + digits[5] + digits[7];
 
-           
             if ((((teklerinToplami * 7) - ciftlerinToplami) % 10) != digits[9]) return false;
-            
+
+            // 5 – İlk 10 hanenin toplamından elde edilen sonucun 10’a bölümünden kalan sayı (MOD10) 11. basamaktaki sayıyı vermelidir.
             int ilkOnToplami = digits.Take(10).Sum();
             if ((ilkOnToplami % 10) != digits[10]) return false;
 
